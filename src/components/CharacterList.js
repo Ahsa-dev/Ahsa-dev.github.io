853ed093b053eec9e78618a0f4c20bf766edcb8c
Ahsa-dev <juancg364@gmail.com>
2021-07-27 01:09:40 -0500

    Style: edit code break lines

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -5,16 +5,27 @@ import Paginator from './Paginator';
 const  CharacterList = (props) => {
 
     const apiUrl = "https://rickandmortyapi.com/api/character/?page=";
-    const [state, setState] = useState({data: null, actualPage: 1, pages: 0});
+    const [state, setState] = useState({
+        data: null,
+        actualPage: 1,
+        pages: 0,
+        loading: true
+    });
 
     useEffect(()=> {
         getCharacters();
     },[state.actualPage]);
 
     const getCharacters = async () => {
+        setState({...state, loading: true });
         try {
             const data = await api.data.getData(apiUrl+state.actualPage);
-            setState({...state, data: data.results, pages: data.info.pages });
+            setState({
+                ...state,
+                data: data.results,
+                pages: data.info.pages,
+                loading: false,
+            });
         }catch(e){
             console.log('error',e);
         }
@@ -26,7 +37,7 @@ const  CharacterList = (props) => {
 
     return (
         <>
-            <div className="characters">
+            <div className={`characters ${state.loading ? 'loading':'loaded'}`}>
                 {state.data !==null &&
                     state.data.map(function(current,index){
                         return (
