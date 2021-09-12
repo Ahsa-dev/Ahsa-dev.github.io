3a754ad8aa3441251c08d7bab4c3f77c277404ab
Ahsa-dev <juancg364@gmail.com>
2021-09-12 00:36:45 -0500

    Feat: add styles for app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './styles/App.css';
 import FrontPage from './pages/FrontPage.js';
-import CharacterDetail from './pages/CharacterDetail.js'
+import CharacterDetail from './pages/CharacterDetail.js';
 import HomePage from './pages/HomePage';
+import { AppStyle } from './styles/modules/app.module.scss';
 import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className={AppStyle}>
       <Route exact path="/" render={HomePage}/>
       <Route exact path="/page/:id" render={FrontPage}/>
       <Route exact path="/character/:id" render={CharacterDetail}/>
